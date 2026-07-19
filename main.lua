4043a480d915c7c6488d505e924ba126e7716e72
Alfredo Martinez <alfredosrmartinez@gmail.com>
2026-07-19 00:45:55 -0600

    abstraction complete

--- main.lua
+++ main.lua
@@ -38,7 +38,7 @@ function love.load()
 
 	berry = new_berry(love.math.random(25,screen_width), love.math.random(25,screen_height), 20, 20, 1, '@')
 
-	evil_berry = new_berry(love.math.random(25,screen_width-100), love.math.random(25,screen_height-100), 20, 20, -1, '&')
+	evil_berry = new_berry(-10, -10, 20, 20, -1, '&')
 
 	input = true
 	timer = 0
@@ -57,6 +57,20 @@ function love.update(dt)
     	love.event.quit()
    	end
 
+   	if love.keyboard.isDown('r') then
+    	-- ¡La fábrica nos da una serpiente nuevecita y limpia!
+    	snake = new_snake("right", 25, 25, 20, 20, 100) 
+    	score = 0
+    	input = true
+    	game_state = "game_on"
+
+    	berry.x_position = love.math.random(25,screen_width)
+		berry.y_position = love.math.random(25,screen_height)
+
+		evil_berry.x_position = -10
+		evil_berry.y_position = -10
+   	end
+
    	if love.keyboard.isDown('o') then
     	score = score + 5
    	end
@@ -70,6 +84,43 @@ function love.update(dt)
    		game_state = "epic_victory"
    		input = false
    		epic_victory:play()
+   	end
+
+   	-- Revisar si la serpiente choca con su propio cuerpo
+	for i = 5, #snake.snake_segments do
+    	if CheckCollision(snake.x_position, snake.y_position, snake.width, snake.height, snake.snake_segments[i].x_position, snake.snake_segments[i].y_position, 20, 20) then
+        	game_state = "game_over"
+        	snake.current_direction = "stop"
+        	input = false
+        	damage_taken:play()
+        end
+    end
+
+   	if(CheckCollision(snake.x_position,snake.y_position,snake.width,snake.height, berry.x_position,berry.y_position,berry.berry_width,berry.berry_height)) then
+			score = score + berry.berry_score
+			print("score = " .. score)
+			berry.x_position = love.math.random(25,screen_width)
+			berry.y_position = love.math.random(25,screen_height)
+			berry_eaten:play()
+		if (score == 10) then
+    		evil_berry.x_position = love.math.random(25,screen_width-100)
+			evil_berry.y_position = love.math.random(25,screen_height-100)
+   		end
+	end
+
+	if(CheckCollision(snake.x_position,snake.y_position,snake.width,snake.height, evil_berry.x_position,evil_berry.y_position,evil_berry.berry_width,evil_berry.berry_height)) then
+			score = score + evil_berry.berry_score -- Aquí cambiamos a suma
+			print("score = " .. score)
+			evil_berry.x_position = love.math.random(25, screen_width-100)
+			evil_berry.y_position = love.math.random(25, screen_height-100)
+			berry_eaten:play()
+	end
+
+   	if(snake.x_position <= 22 or snake.y_position <= 22 or snake.x_position >= 757 or snake.y_position >= 546) and game_state == "game_on" then
+   		game_state = "game_over"
+   		snake.current_direction = "stop"
+   		input = false
+   		damage_taken:play()
    	end
 end
 
